52377128334c24f2dfba903517d121a10c9355de
babotavo419 <one.faith419@gmail.com>
2023-06-02 19:33:33 -0400

    ok

--- src/Components/valadation/formSchema.js
+++ src/Components/valadation/formSchema.js
@@ -1,11 +1,11 @@
 import * as yup from 'yup';
 
 const schema = yup.object().shape({
-  Name: yup
-  .string()
-  .required('Name is required')
-  .min(2, 'Name must be at least 2 characters'),
+    Name: yup
+      .string()
+      .required(['Name is required'])
+      .min(2, ['Name must be at least 2 characters']),
+  });
   
-});
 
 export default schema;
